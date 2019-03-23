1b9d4142ab1a5aa82561278b7a1df8f9d9281d45
haominyuan <tstxxy@gmail.com>
2019-03-23 15:46:04 +0800

    因为长度是 2 的幂，所以可以用 lg2 来存储每个节点的长度，因此每个节点改为用一个字节来存储

--- include/buddy.c
+++ include/buddy.c
@@ -1,12 +1,11 @@
 #include "buddy.h"
 #include <stdlib.h>
 #include <assert.h>
-#include <stdio.h>
 #include <string.h>
 
 struct buddy {
-	unsigned size;
-	unsigned longest[1];
+	char size;
+	char longest[1];
 };
 
 #define LEFT_LEAF(index) ((index) * 2 + 1)
@@ -25,24 +24,38 @@ unsigned fix_size(unsigned size) {
 	return size + 1;
 }
 
+static char convert(int size) {
+	if (size <= 0)
+		return -1;
+	int left = 0, right = 31, mid = 0;
+	while (left < right) {
+		mid = left + (right - left) / 2;
+		if ((1 << mid) < size) {
+			left = mid + 1;
+		} else {
+			right = mid;
+		}
+	}
+	return (char) left;
+}
+
 struct buddy *buddy_new(int size) {
 	struct buddy *self;
-	unsigned node_size;
-	int i;
+	int node_size, i;
 	
 	if (size < 1 || !IS_POWER_OF_2(size)) {
 		return NULL;
 	}
 
-	self = (struct buddy *) malloc(2 * size * sizeof(unsigned));
+	self = (struct buddy *) malloc(2 * size * sizeof(char));
 
-	self->size = (unsigned int) size;
-	node_size = (unsigned int) (size * 2);
+	self->size = convert(size);
+	node_size = self->size + 1;
 
 	for (i = 0; i < 2 * size - 1; i++) {
 		if (IS_POWER_OF_2(i + 1))
-			node_size = node_size / 2;
-		self->longest[i] = node_size;
+			node_size = node_size - 1;
+		self->longest[i] = (char) node_size;
 	}
 
 	return self;
@@ -54,11 +67,11 @@ void buddy_destory(struct buddy *self) {
 
 
 int buddy_alloc(struct buddy *self, int size) {
-	unsigned index = 0;
-	unsigned node_size;
-	unsigned offset = 0;
+	unsigned index = 0, offset = 0;
+	char node_size;
+	int lg_size;
 
-	if (self == NULL) {
+	if (self == NULL || size == 0) {
 		return -1;
 	}
 
@@ -66,19 +79,21 @@ int buddy_alloc(struct buddy *self, int size) {
 		size = fix_size((unsigned int) size);
 	}
 
-	if (self->longest[index] < size)
+	lg_size = convert(size);
+
+	if (self->longest[index] < lg_size)
 		return -1;
 
-	for (node_size = self->size; node_size != size; node_size /= 2) {
-		if (self->longest[LEFT_LEAF(index)] >= size) {
+	for (node_size = self->size; node_size != lg_size; node_size -= 1) {
+		if (self->longest[LEFT_LEAF(index)] >= lg_size) {
 			index = LEFT_LEAF(index);
 		} else {
 			index = RIGHT_LEAF(index);
 		}
 	}
 
-	self->longest[index] = 0;
-	offset = (index + 1) * node_size - self->size;
+	self->longest[index] = -1;
+	offset = (index + 1) * (1 << node_size) - (1 << self->size);
 
 	while (index != 0) {
 		index = PARENT(index);
@@ -88,31 +103,31 @@ int buddy_alloc(struct buddy *self, int size) {
 }
 
 void buddy_free(struct buddy *self, int offset) {
-	unsigned node_size, index = 0;
-	unsigned left_longest, right_longest;
+	int node_size, index;
+	char left_longest, right_longest;
 
 	assert(self && offset >= 0 && offset < self->size);
 
-	node_size = 1;
-	index = self->size - 1 + offset;
+	node_size = 0;
+	index = (1 << self->size) - 1 + offset;
 
-	for (; self->longest[index]; index = PARENT(index)) {
-		node_size = node_size * 2;
+	for (; self->longest[index] != -1; index = PARENT(index)) {
+		node_size = node_size + 1;
 		if (index == 0)
 			return;
 	}
 
-	self->longest[index] = node_size;
+	self->longest[index] = (char) node_size;
 
 	while (index) {
 		index = PARENT(index);
-		node_size = node_size * 2;
+		node_size = node_size + 1;
 
 		left_longest = self->longest[LEFT_LEAF(index)];
 		right_longest = self->longest[RIGHT_LEAF(index)];
 
-		if (left_longest + right_longest == node_size) {
-			self->longest[index] = node_size;
+		if (left_longest == right_longest && node_size - left_longest == 1) {
+			self->longest[index] = (char) node_size;
 		} else {
 			self->longest[index] = MAX(left_longest, right_longest);
 		}
@@ -124,8 +139,8 @@ int buddy_size(struct buddy *self, int offset) {
 	assert(self && offset >= 0 && offset < self->size);
 
 	node_size = 1;
-	for (index = offset + self->size - 1; self->longest[index]; index = PARENT(index)) {
-		node_size = node_size * 2;
+	for (index = (1 << self->size) - 1; self->longest[index] != -1; index = PARENT(index)) {
+		node_size = node_size + 1;
 	}
-	return node_size;
+	return 1 << node_size;
 }
--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 #include "buddy.h"
 
 int main(int argc, char *argv[]) {
-	char cmd[80] = "alloc";
-	int arg = 8;
+	char cmd[80];
+	int arg;
 	struct buddy *b = buddy_new(32);
-	while (1) {
+	puts("Enter q to quit");
+	for (;;) {
+		scanf("%s %d", cmd, &arg);
+		printf("---%d\n", arg);
 		if (strcmp(cmd, "alloc") == 0) {
 			printf("allocted@%d\n", buddy_alloc(b, arg));
 		} else if (strcmp(cmd, "free") == 0) {
 			buddy_free(b, arg);
 		} else if (strcmp(cmd, "size") == 0) {
 			printf("size: %d\n", buddy_size(b, arg));
+		} else if (strcmp(cmd, "q") == 0) {
+			break;
 		}
-		break;
 	}
 	return 0;
 }
